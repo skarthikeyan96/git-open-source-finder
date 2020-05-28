99a7b867d0eb957a08cdfc861eaed20bc86c3d64
karthikeyan <karthikeyan@bangthetable.com>
2020-05-28 22:51:34 +0530

    Feat : Updates and refactoring
    1. Adding standard for linting
    2. refactoring the code and modularization
    3. removing the dark mode in the table
    4. Adding the number of results

--- src/App.js
+++ src/App.js
@@ -1,55 +1,10 @@
 import React from 'react'
-// import ReactMarkdown from 'react-markdown'
-
-import 'bulma/css/bulma.min.css'
+import Pagination from './pagination'
+import Collections from './collections'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 const fetch = require('isomorphic-fetch')
-const BASE_URL = 'https://github.com'
-const Collections = ({issues})=>{
-  function createMarkup(html) {
-    return {__html: html};
-  }
 
-  if(issues.length < 0){
-    return('No issues found')
-  }
-  return(
-    <div class="section table-container">
-      <table class="table">
-        <thead>
-          <th> Issue no</th>
-          <th> Issue Title</th>
-          <th> Repository </th>
-          <th> Issue state </th>
-          <th> Updated Date </th>
-        </thead>
-        <tbody>
-          {
-            issues.map((issue,index) => {
-              const repo = issue.repository_url.split('/')
-              const owner_name = `${repo[repo.length - 2]}`
-              const repo_name = `${repo[repo.length - 1]}`
-
-
-              const updated_Date = `${new Date(issue.updated_at).getDate()}/${new Date(issue.updated_at).getMonth()}/${new Date(issue.updated_at).getFullYear()}`
-              const labels = issue.labels
-              return(
-                <tr>
-                  <th> <p style={{fontWeight:'bold'}}> {index} </p> </th>
-                  <td> {issue.title} </td>
-                  <td> <a href={`${BASE_URL}/${owner_name}/${repo_name}`} target='_blank'>{`${owner_name}/${repo_name}`}</a> </td>
-                  <td> {issue.state} </td>
-
-                  <td> {updated_Date} </td>
-                </tr>
-              )
-            })
-          }
-        </tbody>
-      </table>
-    </div>
-  )
-}
 class App extends React.Component {
   constructor (props) {
     super(props)
@@ -59,107 +14,231 @@ class App extends React.Component {
       languageSearch: '',
       defaultSelect: 'beginner',
       selectChange: false,
-      loading: false
+      loading: false,
+      totalCount: 0,
+      initialPage: 1,
+      nextPage: false,
+      prevPage: false
     }
   }
 
-  handleSelect = (e) =>{
-
-    this.setState({
-      selectChange : !this.state.selectChange,
-      defaultSelect: e.target.value
-    })
-  }
-
-   Setlanguage = (e) => {
-    this.setState({
-      languageSearch: e.target.value
-    })
-  }
-
-   searchRepo = (e) => {
-    const language = this.state.languageSearch.toLowerCase()
-
-    if (e.key === 'Enter') {
-      this.setState({
-        languageSearchTerm: !this.state.languageSearchTerm
+  componentDidMount () {
+    fetch(
+      `https://api.github.com/search/issues?q=language:${this.state.languageSearch.toLowerCase()}+label:${
+        this.state.defaultSelect
+      }+state:open&sort=created&order=desc&page=${this.state.initialPage}`
+    )
+      .then((d) => d.json())
+      .then((r) => {
+        this.setState({
+          issues: r.items,
+          totalCount: r.total_count
+        })
+        console.log(r)
       })
-      fetch(`https://api.github.com/search/issues?q=language:${language}+label:${this.state.defaultSelect}+state:open&sort=created&order=desc`)
-          .then((d) => d.json())
-          .then(r => {
-            this.setState({
-              issues: r.items
-            })
-            console.log(r)
+  }
+
+  componentDidUpdate (prevprops, prevstate) {
+    if (
+      prevstate.defaultSelect !== this.state.defaultSelect ||
+      prevstate.languageSearchTerm !== this.state.languageSearchTerm
+    ) {
+      this.setState(
+        {
+          selectChange: !this.state.selectChange
+        },
+        () => {}
+      )
+      fetch(
+        `https://api.github.com/search/issues?q=language:${this.state.languageSearch.toLowerCase()}+label:${
+          this.state.defaultSelect
+        }+state:open&sort=created&order=desc`
+      )
+        .then((d) => d.json())
+        .then((r) => {
+          this.setState({
+            issues: r.items,
+            totalCount: r.total_count
+          })
         })
     }
   }
 
-  render () {
-    const language = this.state.languageSearch.toLowerCase()
-    // https://api.github.com/search/issues?q=language:python+label:easy+state:open&sort=created&order=desc
-    const renderTags = (e) => {
-      if (this.state.languageSearchTerm) {
-        
+  handleChange = (e) => {
+    this.setState({
+      selectChange: !this.state.selectChange,
+      defaultSelect: e.target.value
+    })
+  }
+
+  handleLang = (e) => {
+    this.setState({
+      languageSearch: e.target.value
+    })
+  };
+
+  handleEnter = (e) => {
+    if (e.key === 'Enter') {
+      this.setState({
+        languageSearchTerm: !this.state.languageSearchTerm
+      })
+    }
+  };
+
+  onhandleNextPage = (e) => {
+    const perPageCount = 30
+    const totalCount = this.state.totalCount ? this.state.totalCount : 0
+    const noOfPages = Math.ceil(totalCount / perPageCount)
+
+    if (this.state.initialPage <= noOfPages) {
+      this.setState(
+        {
+          initialPage: this.state.initialPage + 1
+        },
+        () => {
+          fetch(
+            `https://api.github.com/search/issues?q=language:${this.state.languageSearch.toLowerCase()}+label:${
+              this.state.defaultSelect
+            }+state:open&sort=created&order=desc&page=${this.state.initialPage}`
+          )
+            .then((d) => d.json())
+            .then((r) => {
+              this.setState({
+                issues: r.items,
+                nextPage: !this.state.nextPage
+              })
+            })
+        }
+      )
+    }
+  };
+
+  onhandlePrevPage = (e) => {
+    console.log(e.target)
+
+    if (this.state.initialPage > 1) {
+      console.log('inside the state')
+      this.setState({
+        initialPage: this.state.initialPage - 1
+      })
+    }
+  };
+
+   renderTags = (e) => {
+     const language = this.state.languageSearch.toLowerCase()
+     return (
+       <div className='container-fluid'>
+         {language.length > 0 ? (
+           <button className='btn btn-primary'>
+             {' '}
+             {language}{' '}
+             <span className='badge'> {this.state.totalCount} </span>{' '}
+           </button>
+         ) : (
+           <> </>
+         )}
+
+         <button className='btn btn-dark issue-label'>
+           {' '}
+           {this.state.defaultSelect}{' '}
+           <span className='badge badge-danger'>
+             {' '}
+             {this.state.totalCount}{' '}
+           </span>{' '}
+         </button>
+       </div>
+     )
+   };
+
+    count = () => {
+      const perPageCount = 30
+
+      if (this.state.nextPage) {
         return (
-          <div className='container'>
-           
-            <button className='button language'> {language} </button>
-           
-            <button className='button issue-label'> {this.state.defaultSelect} </button>
-          </div>
+          <p>
+            {' '}
+            Showing {perPageCount + perPageCount} of {this.state.totalCount}{' '}
+            results{' '}
+          </p>
         )
       }
-    }
-    
-    const labels = [
-      'beginner', 'easy', 'starter', 'good first bug', 'low hanging fruit', 'bitesize', 'trivial', 'easy fix', 'new contributor'
-    ]
-    return (
-      <div>
-        <nav class='navbar' role='navigation' aria-label='main navigation'>
-          <div class='navbar-brand'>
-            <a class='navbar-item' href='#'>
-              {'Open Source Finder'.toUpperCase()}
-            </a>
+      return (
+        <p>
+          {' '}
+          Showing {perPageCount} of {this.state.totalCount} results{' '}
+        </p>
+      )
+    };
+
+    render () {
+      const labels = [
+        'beginner',
+        'easy',
+        'starter',
+        'good first bug',
+        'low hanging fruit',
+        'bitesize',
+        'trivial',
+        'easy fix',
+        'new contributor'
+      ]
+
+      return (
+        <div>
+          <div className='container-fluid'>
+            <nav className='navbar navbar-expand-lg fixed-top navbar-light bg-dark'>
+              <a className='navbar-brand' href='#' style={{ color: 'white' }}>
+                {'Open Source Finder'.toUpperCase()}
+              </a>
+            </nav>
           </div>
-        </nav>
-        <div className='section columns'>
-          <div class='column is-half'>
-            <input
-              type='text' className='input' placeholder='Enter programming language'
-              onChange={this.Setlanguage}
-              onKeyDown={this.searchRepo}
-            />
-          </div>
-          <div class='column is-half'>
-            <div class='select'>
-              <select
-                value={this.state.selectValue}
-                onChange={this.handleSelect}
-              >
-                {
-                  labels.map(label => {
-                    return (<option key={label}> {label} </option>)
-                  })
-                }
-              </select>
+
+          <div className='container-fluid'>
+            <div className='row'>
+              <div className='col-md-6'>
+                <input
+                  type='text'
+                  className='form-control'
+                  placeholder='Enter programming language'
+                  onChange={this.handleLang}
+                  onKeyDown={this.handleEnter}
+                />
+              </div>
+              <div className='col-md-6'>
+                <select
+                  value={this.state.selectValue}
+                  onChange={this.handleChange}
+                  className='form-control'
+                >
+                  {labels.map((label) => {
+                    return <option key={label}> {label} </option>
+                  })}
+                </select>
+              </div>
             </div>
           </div>
+
+          <br />
+
+          {this.renderTags()}
+
+          <hr />
+
+          <div className='container-fluid'>{this.count()}</div>
+
+          <Collections
+            issues={this.state.issues}
+            selectionLabel={this.state.defaultSelect}
+          />
+          <Pagination
+            totalCount={this.state.totalCount}
+            handleNextPage={this.onhandleNextPage}
+            handlePrevPage={this.onhandlePrevPage}
+            initialPage={this.state.initialPage}
+          />
         </div>
-
-        {
-          renderTags()
-        }
-
-        
-
-        <hr />
-  
-        <Collections issues = {this.state.issues}/>
-      </div>
-    )
-  }
+      )
+    }
 }
 
 export default App
